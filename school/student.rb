304eea2ea8273fb081501aa94d7b6e3e85a702fd
jenmcphail <mcphail.jen@gmail.com>
2016-09-09 18:53:55 -0700

    passes all tests

--- school/student.rb
+++ school/student.rb
@@ -1,3 +1,19 @@
 class Student
-  
+  attr_reader :name
+
+  @@students = []
+
+  def initialize(name)
+    @name=name
+    @@students << self
+  end
+
+  def self.count
+    @@students.count
+  end
+
+  def self.reset_all
+    @@students.clear
+  end
+
 end
